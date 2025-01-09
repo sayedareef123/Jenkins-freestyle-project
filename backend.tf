ab82cfb9cd61acca6b9165895297ca2720d24c84
Sayed Areef <sayedareef123@gmail.com>
2025-01-09 16:16:02 +0530

    adding aditional one

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "github-bucket-wordpress-websit" 
+    bucket         = "wordpress-websit" 
     key            = "actions/terraform.tfstate"    # Path to the state file in S3
     region         = "ap-south-1"                 # AWS region
     encrypt        = true                         # Enable encryption
