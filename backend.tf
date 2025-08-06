b66ae53286fc87108e71577adc6f2243f7a97b98
sayedareef123 <103107800+sayedareef123@users.noreply.github.com>
2025-08-06 10:08:20 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket         = "wordpress-websit" 
-    key            = "actions/terraform.tfstate"    # Path to the state file in S3
-    region         = "ap-south-1"                 # AWS region
-    encrypt        = true                         # Enable encryption
-  }
-}
+#terraform {
+ # backend "s3" {
+   #key            = "actions/terraform.tfstate"    # Path to the state file in S3
+    #region         = "ap-south-1"                 # AWS region
+    #encrypt        = true                         # Enable encryption
+  #}
+#}
